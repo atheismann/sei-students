018bb5c3d785b32d428f4f7e3d6c65020113b316
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-21 15:15:10 -0500

    code verify

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var Student = require('../models/student');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -7,6 +8,21 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in with OAuth...
-  }
+    Student.findOne({ 'googleId': profile.id }, function(err, student) {
+      if (err) return cb(err);
+      if (student) {
+        return cb(null, student);
+      } else {
+        var newStudent = new Student({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
+        });
+        newStudent.save(function(err) {
+          if (err) return cb(err);
+          return cb(null, newStudent);
+        });
+      }
+    });
+  };
 ));
